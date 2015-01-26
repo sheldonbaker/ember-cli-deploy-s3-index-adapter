794ff399c10cd0376fa6cfe21cbe6955904cc21a
Sheldon Baker <sheldonnbbaker@gmail.com>
2015-01-26 12:26:56 -0800

    rename again?

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var S3IndexAdapter = require('./lib/s3-index-adapter');
 
 function EmberCLIDeployS3IndexAdapter() {
-  this.name = 'aaa-ember-cli-deploy-s3-index-adapter';
+  this.name = 'ember-cli-deploy-aaas3-index-adapter';
   this.adapter = S3IndexAdapter;
 }
 
