5af5f835e065ebbf44a25ac7673a71b567ac6fea
Sheldon Baker <sheldonnbbaker@gmail.com>
2015-01-26 11:53:54 -0800

    rename to be first?

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var S3IndexAdapter = require('./lib/s3-index-adapter');
 
 function EmberCLIDeployS3IndexAdapter() {
-  this.name = 'ember-cli-deploy-s3-index-adapter';
+  this.name = 'aaa-ember-cli-deploy-s3-index-adapter';
   this.adapter = S3IndexAdapter;
 }
 
